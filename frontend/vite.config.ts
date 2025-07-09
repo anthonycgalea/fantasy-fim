40959e590a185c109b9ba48e9bef6d4c53d25562
anthonycgalea <44701741+anthonycgalea@users.noreply.github.com>
2025-07-09 17:16:44 -0400

    change build output

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [
     TanStackRouterVite(),
     react()
