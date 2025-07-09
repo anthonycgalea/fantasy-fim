14d87097855ce32e15d0f9509245e4ee0a9d47f9
Anthony Galea <44701741+anthonycgalea@users.noreply.github.com>
2025-07-09 11:03:59 -0400

    Add Event Data endpoint and page

---
+++ frontend/src/types/EventData.ts
@@ -0,0 +1,9 @@
+export type EventData = {
+  event_name: string;
+  teamcount: number;
+  maxepa: number | null;
+  top8epa: number | null;
+  top24epa: number | null;
+  avgepa: number | null;
+  medianepa: number | null;
+};
