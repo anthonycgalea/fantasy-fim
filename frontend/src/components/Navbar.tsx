0fe80b0666e785acd443d9ba82193fbbec599b83
anthonycgalea <44701741+anthonycgalea@users.noreply.github.com>
2025-07-09 10:12:58 -0400

    Navbar ported

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { Link } from "@tanstack/react-router";
+
+export const Navbar = () => {
+  return (
+    <nav className="w-full py-4 mb-4 border-b">
+      <ul className="flex gap-4">
+        <li>
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/eventData" className="hover:underline">
+            Events
+          </Link>
+        </li>
+        <li>
+          <a href="/apidocs" className="hover:underline">
+            API
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/anthonycgalea/fantasy-fim"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            GitHub
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Navbar from "@/components/Navbar";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <div className="flex flex-col min-h-screen bg-background px-8 mt-4">
+        <Navbar />
         <Outlet />
       </div>
       <TanStackRouterDevtools />
