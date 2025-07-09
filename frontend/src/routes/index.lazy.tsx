54f89a0ea8246f4697ae205423c0df8d6ebd3d1e
Anthony Galea <44701741+anthonycgalea@users.noreply.github.com>
2025-07-09 15:33:35 -0400

    Add landing page and navbar logo

--- frontend/src/routes/index.lazy.tsx
+++ frontend/src/routes/index.lazy.tsx
@@ -5,5 +5,13 @@ export const Route = createLazyFileRoute("/")({
 });
 
 function Index() {
-  return <div className="p-2"></div>;
+  return (
+    <div className="p-2 space-y-4">
+      <h1 className="text-2xl font-bold">Welcome to Fantasy FiM!</h1>
+      <p>
+        Fantasy FiM lets you create leagues, draft robotics teams and track
+        their progress in FIRST in Michigan competitions.
+      </p>
+    </div>
+  );
 }
